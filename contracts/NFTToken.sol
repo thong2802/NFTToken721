17cc913fd18cfe27844dd59618650f5642c9b81c
thong2802 <ducthong2802@gmail.com>
2022-05-25 13:25:16 +0700

    first commit

---
+++ contracts/NFTToken.sol
@@ -0,0 +1,27 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 < 0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract ERC721token is ERC721{
+    using Strings for uint256;
+    string public baseURI = ;
+
+    constructor() ERC71( "NFT Demo", "NFTDT") {
+
+    }
+    function tokenURL(uint256 tokenID) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        string memory buri = baseURI;
+        return bytes(buri).length > 0
+            ? string(abi.encodePacked(buri, tokenId.toString(), ".json"))
+            : '';
+    }
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+
+}
